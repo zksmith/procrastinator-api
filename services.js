45394aef32198702544acb855139011abb033192
zksmith <zahiersmith96@gmail.com>
2020-06-11 18:19:28 -0400

    nyt api update

--- services.js
+++ services.js
@@ -57,8 +57,7 @@ const nytApi = async () => {
   const response = await axios.get(
     `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${key}`
   );
-  console.log(response.results);
-  const formattedData = response.results.map((object) => ({
+  const formattedData = response.data.results.map((object) => ({
     title: object.title,
     section: object.section,
     url: object.url,
