d0976fe5e595b8fb9440f16f074642ce2ca4abbf
zksmith <zahiersmith96@gmail.com>
2020-06-12 12:23:25 -0400

    database setup

--- server.js
+++ server.js
@@ -1,13 +1,84 @@
 const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+var knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    connectString: process.env.DATABASE_URL,
+    ssl: true,
+  },
+});
 
 const data = require('./controllers/data');
 
 const app = express();
+app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
   res.json(':)');
+});
+
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+
+  if (!email || !name || !password) {
+    return res.status(400).json('incorrect form submission');
+  }
+  const hash = bcrypt.hashSync(password);
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into('login')
+      .returning('email')
+      .then((loginEmail) => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            name: name,
+            email: loginEmail[0],
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json('unable to register'));
+});
+
+app.post('/signin', (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json('incorrect form submission');
+  }
+
+  db.select('email', 'hash')
+    .from('login')
+    .where('email', '=', email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(password, data[0].hash);
+      if (isValid) {
+        return db
+          .select('*')
+          .from('users')
+          .where('email', '=', email)
+          .then((user) => {
+            res.json(user[0]);
+          })
+          .catch((err) => res.status(400).json('unable to get user'));
+      } else {
+        res.status(400).json('wrond credentials');
+      }
+    })
+    .catch((err) => res.status(400).json('wrong credentials'));
 });
 
 app.get('/allposts', (req, res) => {
