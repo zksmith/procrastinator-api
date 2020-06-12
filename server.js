60cf3bb151d07b0d9153c95d4e1be85b6787fe38
zksmith <zahiersmith96@gmail.com>
2020-06-12 12:55:05 -0400

    add line to fix ssl error

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 var knex = require('knex');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const db = knex({
   client: 'pg',
   connection: {
