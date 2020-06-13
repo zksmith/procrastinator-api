333abf7fdca75169f6383ce63b185da55b5a88e8
zksmith <zahiersmith96@gmail.com>
2020-06-13 02:28:57 -0400

    data test

--- controllers/data.js
+++ controllers/data.js
@@ -45,7 +45,29 @@ const sendTwitchData = async (req, res) => {
   }
 };
 
+const sendAllData = async (req, res) => {
+  try {
+    const redditData = await services.redditApi();
+    const hackerNewsData = await services.hackerNewsApi();
+    const githubData = await services.githubApi();
+    const nytData = await services.nytApi();
+
+    //Randomize the data array using "Knuth Shuffle"
+    const shuffled = shuffle([
+      ...redditData,
+      ...hackerNewsData,
+      ...githubData,
+      ...nytData,
+    ]);
+
+    res.json(shuffled);
+  } catch (err) {
+    res.status(500).json('Error loading data');
+  }
+};
+
 module.exports = {
+  sendAllData,
   sendRedditData,
   sendHackerNewsData,
   sendGithubData,
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const knex = require('knex');
 const signin = require('./controllers/signin');
 const register = require('./controllers/register');
 const bookmark = require('./controllers/bookmark');
+const data = require('./controllers/data');
 const allposts = require('./controllers/allposts');
 const reddit = require('./controllers/reddit');
 const hackernews = require('./controllers/hackernews');
@@ -38,15 +39,20 @@ app.post('/signin', (req, res) => signin.handleSignin(req, res, db, bcrypt));
 
 app.put('/bookmark', (req, res) => bookmark.handleBookmark(req, res, db));
 
-app.get('/allposts', (req, res) => allposts.handleAllRequest(req, res));
+app.get('/allposts', (req, res) => data.sendAllData(req, res));
+
 app.get('/reddit', (req, res) => reddit.handleRedditRequest(req, res));
+
 app.get('/hackernews', (req, res) => hackernews.handleHNRequest(req, res));
+
 app.get('/githubtrending', (req, res) =>
   githubtrending.handleGithubRequest(req, res)
 );
+
 app.get('/newyorktimes', (req, res) => {
   newyorktimes.handleNYTRequest(req, res);
 });
+
 app.get('/twitchstreams', (req, res) => {
   twitchstreams.handleTwitchRequest(req, res);
 });
