135017f300fe0494a9a8f7ed4d7a95c65c4ceb32
zksmith <zahiersmith96@gmail.com>
2020-06-14 23:29:58 -0400

    fixed typo

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,14 @@
 const jwt = require('jsonwebtoken');
 
 const requireAuth = (req, res, next) => {
-  const { authorizaton } = req.headers;
+  const { authorization } = req.headers;
 
-  if (!authorizaton) {
+  if (!authorization) {
+    return res.status(401).json('Unauthorized');
+  }
+
+  const jwtToken = authorizaton.slice(7, authorizaton.length);
+  if (!jwt.verify(jwtToken, process.env.JWT_KEY)) {
     return res.status(401).json('Unauthorized');
   }
 
